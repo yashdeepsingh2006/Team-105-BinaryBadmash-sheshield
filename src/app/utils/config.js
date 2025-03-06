0dd8cc8a4b044c163916aec9124d126398038410
yashdeepsingh2006 <ytharman2006@gmail.com>
2025-03-06 10:35:45 +0530

    deploying

--- src/app/utils/config.js
+++
@@ -1,8 +0,0 @@
-"server only"
-
-import { PinataSDK } from "pinata-web3"
-
-export const pinata = new PinataSDK({
-  pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
-})
